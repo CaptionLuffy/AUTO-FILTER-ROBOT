ae0234c10a914a68adfb3e259ec5b5f806255b7b
CaptionLuffy <159707662+CaptionLuffy@users.noreply.github.com>
2025-05-09 19:20:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,8 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', 'https://telegram.me/ErenArc')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002520364179'))
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002681141827').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://atlasadmin:zoro7890@cluster0.jcefere.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://atadmin:zoror7890@cluster0.psw6z77.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002520364179'))
